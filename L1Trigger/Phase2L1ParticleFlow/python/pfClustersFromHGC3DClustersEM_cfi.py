082d6b5f746a083a679606b3b172eb8408b818bf
Giovanni <gpetruc@gmail.com>
2019-07-01 16:59:04 +0200

    Fix pfClustersFromHGC3DClustersEM_cfi adding preEmId

--- L1Trigger/Phase2L1ParticleFlow/python/pfClustersFromHGC3DClustersEM_cfi.py
+++ L1Trigger/Phase2L1ParticleFlow/python/pfClustersFromHGC3DClustersEM_cfi.py
@@ -1,10 +1,12 @@
 import FWCore.ParameterSet.Config as cms
 
-pfClustersFromHGC3DClustersEM = cms.EDProducer("PFClusterProducerFromHGC3DClusters",
-    src = cms.InputTag("hgcalBackEndLayer2Producer:HGCalBackendLayer2Processor3DClustering"),
+import L1Trigger.Phase2L1ParticleFlow.pfClustersFromHGC3DClusters_cfi
+
+pfClustersFromHGC3DClustersEM = L1Trigger.Phase2L1ParticleFlow.pfClustersFromHGC3DClusters_cfi.pfClustersFromHGC3DClusters.clone(
     emOnly = cms.bool(True),
     etMin = cms.double(0.0), 
     corrector  = cms.string("L1Trigger/Phase2L1ParticleFlow/data/emcorr_hgc.root"),
+    preEmId  = cms.string(""),
     resol = cms.PSet(
             etaBins = cms.vdouble( 1.900,  2.200,  2.500,  2.800,  2.950),
             offset  = cms.vdouble( 0.651,  0.608,  0.496,  0.532,  0.358),
